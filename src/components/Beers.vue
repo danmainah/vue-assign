b2f6ff96476fd779170af3bee642b7da0a5820c3
danmainah <danielmainawaweru@gmail.com>
2022-04-04 16:53:30 +0300

    Add method to fetch data from API

--- src/components/Beers.vue
+++ src/components/Beers.vue
@@ -10,5 +10,21 @@ export default {
     return {
       beers: [],
     };
+},
+
+methods: {
+    async getData() {
+      try {
+        let response = await fetch("https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6");
+        this.beers = await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+
+  created() {
+    this.getData();
+  },
 }
 </script>
